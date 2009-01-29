ec3c8c57b999c4462d23265a648fcf35551a3d4d
pierre <pruffez@gmail.com>
2009-01-29 03:05:43 +0100

    clean

---
+++ src/tick.sh
@@ -0,0 +1,7 @@
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=EURUSD=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/EURUSD/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=GBPUSD=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/GBPUSD/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=USDCHF=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/USDCHF/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=USDJPY=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/USDJPY/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=EURCHF=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/EURCHF/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=EURGBP=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/EURGBP/`date +%Y%m`
+curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=EURJPY=X&f=sl1d1t1&e=.csv">>/Users/pierre/Data/yahoo_quotes/EURJPY/`date +%Y%m`
